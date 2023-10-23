ad981037fb32aea4e872ca78eac77520a4d8dba3
Gabriel Bessi <85580011+EyzRyder@users.noreply.github.com>
2023-10-23 19:08:21 -0300

    Add files via upload

---
+++ ESP32-BT-PKELEC_CAR.ino
@@ -0,0 +1,163 @@
+#include "BluetoothSerial.h"
+
+BluetoothSerial SerialBT;
+
+#define FORWARD 1
+#define BACKWARD 2
+#define LEFT 3
+#define RIGHT 4
+#define FORWARD_LEFT 5
+#define FORWARD_RIGHT 6
+#define BACKWARD_LEFT 7
+#define BACKWARD_RIGHT 8
+#define TURN_LEFT 9
+#define TURN_RIGHT 10
+#define STOP 0
+
+#define BACK_RIGHT_MOTOR 0
+#define BACK_LEFT_MOTOR 1
+#define FRONT_RIGHT_MOTOR 2
+#define FRONT_LEFT_MOTOR 3
+
+struct MOTOR_PINS
+{
+  int pinIN1;
+  int pinIN2;
+  int pinEn; 
+  int pwmSpeedChannel;
+};
+
+std::vector<MOTOR_PINS> motorPins = 
+{
+  {4, 2, 22, 4},  //BACK_RIGHT_MOTOR
+  {18, 19, 23, 5},  //BACK_LEFT_MOTOR
+  {27, 26, 14, 6},  //FRONT_RIGHT_MOTOR
+  {33, 25, 32, 7},  //FRONT_LEFT_MOTOR   
+};
+
+#define MAX_MOTOR_SPEED 900
+
+const int PWMFreq = 1000; /* 1 KHz */
+const int PWMResolution = 8;
+
+#define SIGNAL_TIMEOUT 1000  // This is signal timeout in milli seconds. We will reset the data if no signal
+unsigned long lastRecvTime = 0;
+
+char receivedChar;
+
+void rotateMotor(int motorNumber, int motorSpeed)
+{
+  if (motorSpeed < 0)
+  {
+    digitalWrite(motorPins[motorNumber].pinIN1, LOW);
+    digitalWrite(motorPins[motorNumber].pinIN2, HIGH);    
+  }
+  else if (motorSpeed > 0)
+  {
+    digitalWrite(motorPins[motorNumber].pinIN1, HIGH);
+    digitalWrite(motorPins[motorNumber].pinIN2, LOW);       
+  }
+  else
+  {
+    digitalWrite(motorPins[motorNumber].pinIN1, LOW);
+    digitalWrite(motorPins[motorNumber].pinIN2, LOW);      
+  }
+  
+  ledcWrite(motorPins[motorNumber].pwmSpeedChannel, abs(motorSpeed));
+}
+
+void setUpPinModes()
+{
+  for (int i = 0; i < motorPins.size(); i++)
+  {
+    pinMode(motorPins[i].pinIN1, OUTPUT);
+    pinMode(motorPins[i].pinIN2, OUTPUT);  
+    //Set up PWM for motor speed
+    ledcSetup(motorPins[i].pwmSpeedChannel, PWMFreq, PWMResolution);  
+    ledcAttachPin(motorPins[i].pinEn, motorPins[i].pwmSpeedChannel);     
+    rotateMotor(i, STOP);  
+  }
+}
+
+void setup() {
+  setUpPinModes();
+
+  Serial.begin(2400);
+  SerialBT.begin("4WH Car");
+
+  //rotateMotor(FRONT_RIGHT_MOTOR, MAX_MOTOR_SPEED);
+  //rotateMotor(FRONT_LEFT_MOTOR, -MAX_MOTOR_SPEED);
+  rotateMotor(BACK_RIGHT_MOTOR, -MAX_MOTOR_SPEED);
+  rotateMotor(BACK_LEFT_MOTOR, MAX_MOTOR_SPEED); 
+}
+
+void Forward(){
+      rotateMotor(FRONT_RIGHT_MOTOR, MAX_MOTOR_SPEED);
+      rotateMotor(BACK_RIGHT_MOTOR, -MAX_MOTOR_SPEED);
+      rotateMotor(FRONT_LEFT_MOTOR, -MAX_MOTOR_SPEED);
+      rotateMotor(BACK_LEFT_MOTOR, MAX_MOTOR_SPEED);  
+}
+void Backward(){
+      rotateMotor(FRONT_RIGHT_MOTOR, -MAX_MOTOR_SPEED);
+      rotateMotor(BACK_RIGHT_MOTOR, -MAX_MOTOR_SPEED);
+      rotateMotor(FRONT_LEFT_MOTOR, -MAX_MOTOR_SPEED);
+      rotateMotor(BACK_LEFT_MOTOR, -MAX_MOTOR_SPEED); 
+}
+void Left(){
+      rotateMotor(FRONT_RIGHT_MOTOR, MAX_MOTOR_SPEED);
+      rotateMotor(BACK_RIGHT_MOTOR, -MAX_MOTOR_SPEED);
+      rotateMotor(FRONT_LEFT_MOTOR, -MAX_MOTOR_SPEED);
+      rotateMotor(BACK_LEFT_MOTOR, MAX_MOTOR_SPEED); 
+}
+void Right(){
+      rotateMotor(FRONT_RIGHT_MOTOR, -MAX_MOTOR_SPEED);
+      rotateMotor(BACK_RIGHT_MOTOR, MAX_MOTOR_SPEED);
+      rotateMotor(FRONT_LEFT_MOTOR, MAX_MOTOR_SPEED);
+      rotateMotor(BACK_LEFT_MOTOR, -MAX_MOTOR_SPEED);  
+}
+void Stop(){
+      rotateMotor(FRONT_RIGHT_MOTOR, STOP);
+      rotateMotor(BACK_RIGHT_MOTOR, STOP);
+      rotateMotor(FRONT_LEFT_MOTOR, STOP);
+      rotateMotor(BACK_LEFT_MOTOR, STOP);  
+}
+void loop() {
+    receivedChar =(char)SerialBT.read();
+
+  if (Serial.available()) {
+    SerialBT.write(Serial.read());
+  
+  }
+  if (SerialBT.available()) {
+     
+    Serial.println("Received:");//print on serial monitor
+    Serial.println(receivedChar);//print on serial monitor    
+    
+    if(receivedChar == 'F')
+    {
+      Forward();
+       
+    }
+    if(receivedChar == 'B')
+    {
+ 
+      Backward(); 
+    }         
+     if(receivedChar == 'L')
+    {
+
+      Left();
+    }        
+    if(receivedChar == 'R')
+    {
+
+      Right(); 
+    }
+    if(receivedChar == 'S')
+    {
+      Stop();
+    }
+  }
+  delay(20);
+}
+ 
